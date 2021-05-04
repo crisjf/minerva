851ac968b5bed9930bef82395252a10e4beffbc6
crisjf <crisjf@mit.edu>
2021-05-04 17:21:13 -0400

    update indicator to change grid color

--- indicators.py
+++ indicators.py
@@ -1,13 +1,16 @@
 import pandas as pd
 import numpy as np
+import jenkspy
 from brix import Indicator
 
 class Giants(Indicator):
     '''
     Indicator that simulates the benefit that university researchers get from being in close proximity to private R&D.
     User can change location of academic departments and of private R&D labs, and the module will compute the research output of academia and display it as the height of each cell.
+
+    color_method can be 'jenks', 'quantile', or 'none'
     '''
-    def setup(self,quietly=True):
+    def setup(self,quietly=True,color_method='jenks'):
         self.quietly = quietly
         if not self.quietly:
             print('Setting up indicator')
@@ -15,12 +18,16 @@ class Giants(Indicator):
         self.local_crs = 'ESRI:102008'
         self.name = 'Knowledge spillovers'
         self.indicator_type = 'grid'
+        self.override_verification = True
+        self.requires_geometry = True
         
         self.gamma = 0.004
         self.beta0 = 0.000
         self.beta1 = 0.06
-        self.scale = 2
         
+        self.color_method = color_method
+        
+        self.scale = 2
         self.background_alpha = 0.5
         self.units_alpha = 0.9
         
@@ -28,6 +35,37 @@ class Giants(Indicator):
         
         self.academic_types = set(['Academic'])
         self.private_types  = set(['Private R&D'])
+        
+        self.n_colors = 3
+        self.breaks = None
+        self.color_palette = None
+        self.academic_color = None
+        self.set_color_palette()
+
+    def set_color_palette(self):
+        '''
+        Sets the color palette to be used according to self.n_colors
+        '''
+        self.Reds = {
+            3: [(254,224,210), (252,146,114), (222,45,38)],
+            4: [(254,229,217), (252,174,145), (251,106,74), (203,24,29)],
+            5: [(254,229,217), (252,174,145), (251,106,74), (222,45,38), (165,15,21)],
+            6: [(254,229,217), (252,187,161), (252,146,114), (251,106,74), (222,45,38), (165,15,21)],
+            7: [(254,229,217), (252,187,161), (252,146,114), (251,106,74), (239,59,44), (203,24,29), (153,0,13)],
+            8: [(255,245,240), (254,224,210), (252,187,161), (252,146,114), (251,106,74), (239,59,44), (203,24,29), (153,0,13)],
+            9: [(255,245,240), (254,224,210), (252,187,161), (252,146,114), (251,106,74), (239,59,44), (203,24,29), (165,15,21), (103,0,13)]
+        }
+
+        color_palette = self.Reds[self.n_colors]
+        self.color_palette = [list(c) for c in color_palette]
+
+    def set_academic_color(self,geogrid_data):
+        '''
+        Sets the default color of academic deparments according to GEOGRID.
+        '''
+        if self.academic_color is None:
+            hex_color = geogrid_data.get_geogrid_props()['types']['Academic']['color'].replace('#','')
+            self.academic_color = list(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
         
     def make_dis_df(self,geogrid_data):
         '''
@@ -52,18 +90,26 @@ class Giants(Indicator):
         '''
         if self.dis is None:
             self.make_dis_df(geogrid_data)
+        self.set_academic_color(geogrid_data)
         final_height_lookup = self.propagate_spillovers(geogrid_data)
+        self.set_breaks(final_height_lookup)
+        
         for cell in geogrid_data:
             if cell['id'] in final_height_lookup.keys():
-                cell['height'] = self.scale*min([1000,final_height_lookup[cell['id']]])
+                h = final_height_lookup[cell['id']]
+                cell['height'] = self.scale*min([1000,h])
+                cell['color'] = self.get_color(h)
             elif cell['name'] == 'Default':
                 cell['height'] = 0
             else:
                 cell['height'] = self.scale*self.base_height
+
             if cell['name']=='Default':
                 cell['color'] = cell['color'][:3]+[int(self.background_alpha*255)]
             else:
                 cell['color'] = cell['color'][:3]+[int(self.units_alpha*255)]
+            if 'geometry' in cell.keys():
+                del cell['geometry']
         return geogrid_data
 
     def propagate_spillovers(self,geogrid_data):
@@ -89,3 +135,27 @@ class Giants(Indicator):
         academic['final'] = academic['height']*np.exp(self.beta0+self.beta1*academic['exp'])
         final_height_lookup = dict(academic[['id','final']].values)
         return final_height_lookup
+
+    def set_breaks(self,final_height_lookup):
+        '''
+        Sets the breaks to be used to color cells.
+        '''
+        if self.color_method == 'jenks':
+            breaks = jenkspy.jenks_breaks(final_height_lookup.values(), nb_class=self.n_colors)
+            self.breaks = np.array(breaks)
+        elif self.color_method == 'quantile':
+            breaks = [np.quantile(list(final_height_lookup.values()),q) for q in np.linspace(0,1,self.n_colors+1)]
+            self.breaks = np.array(breaks)
+        else:
+            self.breaks = None        
+    
+    def get_color(self,h):
+        '''
+        Returns the color for height h according to self.breaks (see self.set_breaks)
+        '''
+        if self.breaks is not None:
+            cat = min(min(np.where(self.breaks>=h)))
+            cell_color = self.color_palette[cat-1]
+        else:
+            cell_color = self.academic_color
+        return cell_color
--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from indicators import Giants
 
 table_name = 'minerva'
 H = Handler(table_name,quietly=False)
-giants = Giants(quietly=False)
+giants = Giants(quietly=False,color_method='quantile')
 H.add_indicator(giants)
 H.listen()
